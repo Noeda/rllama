ebdea727fdc596a3686b52eec0d03891d7a8f9ea
Mikko Juola <mikjuo@gmail.com>
2023-03-18 00:48:44 -0700

    Don't let the crate be built without avx2, avx, etc. or it'll be very slow.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[cfg(not(target_feature = "avx2"))]
+compile_error!("This library assumes availability of AVX and must be compiled with -C target-feature=+sse2,+avx,+fma,+avx2");
+#[cfg(not(target_feature = "sse2"))]
+compile_error!("This library assumes availability of AVX and must be compiled with -C target-feature=+sse2,+avx,+fma,+avx2");
+#[cfg(not(target_feature = "fma"))]
+compile_error!("This library assumes availability of AVX and must be compiled with -C target-feature=+sse2,+avx,+fma,+avx2");
+#[cfg(not(target_feature = "avx"))]
+compile_error!("This library assumes availability of AVX and must be compiled with -C target-feature=+sse2,+avx,+fma,+avx2");
+
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     rllama::rllama_main::main()
 }
